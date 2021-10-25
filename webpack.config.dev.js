126c321daed3e5450e27ebfb540e72376f5e3efb
LuismGil <luism6977@gmail.com>
2021-10-25 11:57:00 -0300

    added enviroments variables

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,10 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -14,6 +11,8 @@ module.exports = {
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
+  mode: 'development',
+  watch: true,
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -66,7 +65,7 @@ module.exports = {
       filename: './index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].[contenthash].css'
+      filename: 'assets/[name].[contenhash].css'
     }),
     new CopyPlugin({
       patterns: [
@@ -77,13 +76,5 @@ module.exports = {
       ]
     }),
     new Dotenv(),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin(),
-    ]
-  }
 }
